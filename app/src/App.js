09b4ac487191298adae54e25b76241c38e97c19d
Michail Shaposhnikov <mishapos@microsoft.com>
2021-05-10 13:08:45 -0700

    init commit

---
+++ app/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Look at this really cool spinny thing we have here. Visible update.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Everything is fine. Just fine. Absolutely fine.
+        </a>
+        <a 
+          className="App-link"
+          href="/.auth/login/aad"
+        >
+        Log in with Azure AD
+        </a>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import logo from './logo.svg';
+import './App.css';
+import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+//import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();

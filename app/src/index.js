d4b3804a1b2dca60c84dc4bbee7ce09f685f8080
Michail Shaposhnikov <mishapos@microsoft.com>
2021-05-11 12:50:07 -0700

    Update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import logo from './logo.svg';
 import App from './App.js';
 import './App.css';
 import * as serviceWorker from './serviceWorker';
-import axios from 'axios';
 //import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
